04ed723c2f8b96adf7dd51f313b4c739d82985b7
Arjun Mohan <mohanarjun.juner@gmail.com>
2021-09-26 08:18:22 +0530

    feat header section

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title:string = 'Bug tracker';
+
+  toggleAddTaskBtn(){
+    console.log("Handle from parent component");
+  }
+
+}
