4b5821e52e030b6de865d36f931e5359c12baee9
Arjun Mohan <mohanarjun.juner@gmail.com>
2021-09-27 18:44:08 +0530

    feat http get service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Task } from "../Task";
-import { TASKS } from "../mock-tasks"
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   // service function
   getTasks() :Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
