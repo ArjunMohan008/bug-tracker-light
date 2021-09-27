ca40096f9c36fc840f3263a52ea51961241ed086
Arjun Mohan <mohanarjun.juner@gmail.com>
2021-09-27 17:18:24 +0530

    feat task service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from "../../Task";
 import { TASKS } from "../../mock-tasks"
+import { TaskService } from "../../services/task.service"
 
 @Component({
   selector: 'app-tasks',
@@ -8,12 +9,14 @@ import { TASKS } from "../../mock-tasks"
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-  
 
-  constructor() { }
+  // define taskService
+  constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
+    // define props to use within the component
+    this.tasks = this.taskService.getTasks();
   }
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 }
